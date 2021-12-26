0f9c8067dc2b2a54e082560699302bf9fe4aeafa
JoshDaBosh <jwanggt@gmail.com>
2021-12-25 21:47:50 -0500

    add some config files and setup precommit for fun

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(1)
+  return <h1>L + ratio</h1>
 }
 
-export default App;
+export default App
